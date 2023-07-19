054b4615e203c320cf2f1a85e5d253d6c33e6137
Eko Maulana Yusuf <eko.yusuf@dataon.com>
2023-07-19 07:49:27 +0700

    add some file

--- resources/views/layout/aplikasi.blade.php
+++ resources/views/layout/aplikasi.blade.php
@@ -14,7 +14,7 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{ url('css/sb-admin-2.min.css')}}" rel="stylesheet">
     <title>index</title>
 </head>
 <body>
@@ -100,7 +100,7 @@
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
                                 <img class="img-profile rounded-circle"
-                                     src="img/undraw_profile.svg">
+                                     src="{{url('img/undraw_profile.svg')}}">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
